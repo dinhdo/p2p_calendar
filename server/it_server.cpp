e4104ec46adf3b08dc408d1536c641695aa0a6bf
Dinh Ngoc Do <ddo@student01.cse.nd.edu>
2014-12-06 13:32:40 -0500

    it_server

--- server/it_server.cpp
+++ server/it_server.cpp
@@ -18,7 +18,9 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <string>
 #include <iostream>
+#include <fstream>
 
 #define PORTNO "1203"
 #define BSIZE 256
@@ -28,10 +30,11 @@ using namespace std;
 int main (int argc, char *argv[]){
 
 	int r, s, rbyte, sbyte;
+	uint32_t msgsize, umsgsize;
 	char const *portno = PORTNO;
 	struct addrinfo hint, *sinfo, *t;
 	struct sockaddr_storage servaddr;
-	char buffer[BSIZE];
+	char buffer[BSIZE], abuffer, cbuffer[BSIZE];
 
 	memset (&hint, 0, sizeof(struct addrinfo));
 	hint.ai_family = AF_UNSPEC;
@@ -90,13 +93,267 @@ int main (int argc, char *argv[]){
 		}
 
 		
-		//Receive Message
-		if(rbyte = read(r, &buffer, sizeof(buffer)) < 0){
+		//Receive Action
+		if(rbyte = read(r, &abuffer, sizeof(char)) < 0){
 				perror("SERVER: Read error\n");
 				return -1;
 		}
 
-		cout << "Message received: " << buffer << endl;
+		cout << "Received action: '" << abuffer << "'"<< endl;
+
+
+		//get length of filename
+		if((rbyte=read(r, &msgsize, sizeof(uint32_t))) < 0){
+				perror("read error: lengthname\n");
+				exit(1);
+		}
+
+		msgsize = ntohl(msgsize);
+		cout << "Received length of filename: " << msgsize << endl;
+
+		bzero(cbuffer, BSIZE);
+		//Receive Calendar Name
+		if(rbyte = read(r, &cbuffer, msgsize) < 0){
+				perror("SERVER: Read error\n");
+				return -1;
+		}
+
+		cout << "Received calendar name: " << cbuffer << endl;
+		char calname[BSIZE];
+		strcpy(calname, cbuffer);
+
+		cout << "Cal name after strcpy: " << calname << endl;
+
+		cout << "abuffer = " << abuffer << endl;
+
+		// Fulfill request
+		if(abuffer == 'A'){
+			
+			cout << "In ADD action!" << endl;
+
+			//Receive XML size
+			if(rbyte = read(r, &msgsize, sizeof(uint32_t)) < 0){
+				perror("Server: Read size error\n");
+				return 1;
+			}
+
+
+			msgsize = ntohl(msgsize);
+			cout << "Read msg size: " << msgsize << endl;
+			
+
+			//Receive XML
+			bzero(buffer, BSIZE);
+			cout << buffer << endl;
+			if(rbyte = read(r, &buffer, msgsize) < 0){
+				perror("SERVER: Read error\n");
+				return -1;
+			}
+
+			buffer[msgsize] = 0;
+			cout << "Received message: " << buffer << endl;
+
+			char insertstr[BSIZE];
+			strcpy(insertstr, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+
+			FILE * xFile;
+
+			//Open file
+			strcat(calname, ".xml");
+
+			cout << "Filename = " << calname << endl;
+	
+			xFile = fopen(calname, "a+");
+
+			cout << "Opened file " << calname << endl;			
+
+
+			//*****Parse through and check if event exists & for conflicts*****
+
+
+			//Append event
+			fputs(insertstr, xFile);
+			fputs("\n", xFile);
+			fputs(buffer, xFile);
+			
+			cout << "Appended event: " << buffer << endl;
+
+			fclose (xFile);
+
+			
+			
+		}else if(abuffer == 'R'){
+	
+			//Receive XML
+			if(rbyte = read(r, &buffer, msgsize) < 0){
+				perror("SERVER: Read error\n");
+				return -1;
+			}
+
+			buffer[msgsize] = 0;
+
+
+			char filename [BSIZE];
+			strcpy(filename, calname);
+			strcat(filename, ".xml");	
+
+			//Try to open file			
+			ifstream ifile(filename);
+
+			//If the file exists
+			if(ifile){
+				
+				//Parse through and find block event that matches date and start above
+				//Remove event *****
+				
+			} else {
+			//If file does not exist; send error message	
+					
+				char error[BSIZE];
+				strcpy(error, "Calendar does not exist. Cannot remove event.");
+				msgsize = strlen(error);
+				
+				//Send message size
+				umsgsize = htonl(msgsize);
+				if(sbyte = write(r, &umsgsize, sizeof(uint32_t)) < 0){
+					perror("Server: Error in sending error message");
+					return 1;
+				}
+
+				//Send error message
+				if(sbyte = write(r, &error, msgsize) < 0){
+					perror("Server: Error in sending error message");
+					return 1;				
+				}
+			} 								
+
+		} else if(abuffer == 'U'){
+
+			//Receive XML
+			if(rbyte = read(r, &buffer, msgsize) < 0){
+				perror("SERVER: Read error\n");
+				return -1;
+			}
+
+			buffer[msgsize] = 0;
+
+			char filename [BSIZE];
+			strcpy(filename, calname);
+			strcat(filename, ".xml");	
+			
+			//Try to open file			
+			ifstream ifile(filename);
+
+			//If the file exists
+			if(ifile){
+
+				//Parse through file and find block event
+				//Update event with new information given
+				
+			} else {
+			//If file does not exist; send error message	
+					
+				char error[BSIZE];
+				strcpy(error, "Calendar does not exist. Cannot remove event.");
+				msgsize = strlen(error);
+				
+				//Send message size
+				umsgsize = htonl(msgsize);
+				if(sbyte = write(r, &umsgsize, sizeof(uint32_t)) < 0){
+					perror("Server: Error in sending error message");
+					return 1;
+				}
+
+				//Send error message
+				if(sbyte = write(r, &error, msgsize) < 0){
+					perror("Server: Error in sending error message");
+					return 1;				
+				}
+			} 
+	
+		} else if(abuffer == 'G'){
+
+			//Receive date
+			if(rbyte = read(r, &buffer, msgsize) < 0){
+				perror("SERVER: Read error\n");
+				return -1;
+			}
+
+			buffer[msgsize] = 0;
+
+			char filename [BSIZE];
+			strcpy(filename, calname);
+			strcat(filename, ".xml");
+
+			//Try to open file			
+			ifstream ifile(filename);
+
+			//If the file exists
+			if(ifile){
+		
+				//Parse through file, locate events with matching date
+				//Send info to client
+				
+			} else {
+			//If file does not exist; send error message	
+					
+				char error[BSIZE];
+				strcpy(error, "Calendar does not exist. Cannot remove event.");
+				msgsize = strlen(error);
+				
+				//Send message size
+				umsgsize = htonl(msgsize);
+				if(sbyte = write(r, &umsgsize, sizeof(uint32_t)) < 0){
+					perror("Server: Error in sending error message");
+					return 1;
+				}
+
+				//Send error message
+				if(sbyte = write(r, &error, msgsize) < 0){
+					perror("Server: Error in sending error message");
+					return 1;				
+				}
+			} 	
+			
+		
+		} else if (abuffer == 'S'){
+
+			char filename [BSIZE];
+			strcpy(filename, calname);
+			strcat(filename, ".xml");	
+			
+			//Try to open file			
+			ifstream ifile(filename);
+
+			//If the file exists
+			if(ifile){
+				//Send all events to client separated by block
+				//1 second intervals
+				
+			} else {
+			//If file does not exist; send error message	
+					
+				char error[BSIZE];
+				strcpy(error, "Calendar does not exist. Cannot remove event.");
+				msgsize = strlen(error);
+				
+				//Send message size
+				umsgsize = htonl(msgsize);
+				if(sbyte = write(r, &umsgsize, sizeof(uint32_t)) < 0){
+					perror("Server: Error in sending error message");
+					return 1;
+				}
+
+				//Send error message
+				if(sbyte = write(r, &error, msgsize) < 0){
+					perror("Server: Error in sending error message");
+					return 1;				
+				}
+			} 
+		
+		} else {
+			perror("Action sent by client unknown!");
+		}		
 
 	}
 }
